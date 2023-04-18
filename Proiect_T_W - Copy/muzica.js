919d677301ea9c23abd38eb11ffeac71b36629d5
Ariana170 <104589322+Ariana170@users.noreply.github.com>
2023-04-18 22:46:29 +0300

    Add files via upload

---
+++ Proiect_T_W - Copy/muzica.js
@@ -0,0 +1,8 @@
+    document.querySelector("#id-checkbox1").addEventListener("click", function(event) {
+        document.getElementById("output-box").innerHTML = "Ha Ha... o sa fie Speak Now";
+        event.preventDefault();
+    }, false);
+    document.querySelector("#id-checkbox2").addEventListener("click", function(event) {
+        document.getElementById("output-box").innerHTML = "Ha Ha... o sa fie 1989";
+        event.preventDefault();
+    }, false);
